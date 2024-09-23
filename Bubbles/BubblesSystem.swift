e750f7b1657f763956aa4131988e9ed2ccb4b72b
sarangborude <sarang.borude@gmail.com>
2024-09-22 18:20:58 -0700

    fix issue with bubbles movement freezing in the system

--- Bubbles/BubblesSystem.swift
+++ Bubbles/BubblesSystem.swift
@@ -10,17 +10,27 @@ import RealityKitContent
 
 class BubblesSystem: System {
     let query = EntityQuery(where: .has(BubbleComponent.self))
-    let speed: Float = 0.0001
+    let speed: Float = 0.1// 0.0001
     
     required init(scene: Scene) {
         
     }
     
     func update(context: SceneUpdateContext) {
-        context.scene.performQuery(query).forEach { bubble in
+        
+        let entities = context.entities(matching: query, updatingSystemWhen: .rendering)
+        
+        for bubble in entities  {
             guard let bubbleComponent = bubble.components[BubbleComponent.self] else {return}
             
-            bubble.position += bubbleComponent.direction * speed
+            bubble.position += bubbleComponent.direction * speed * Float(context.deltaTime)
         }
+        
+        /// For some reason this make the bubbles stop moving  so the above method works better when updating the system
+//        context.scene.performQuery(query).forEach { bubble in
+//            guard let bubbleComponent = bubble.components[BubbleComponent.self] else {return}
+//            
+//            bubble.position += bubbleComponent.direction * speed
+//        }
     }
 }
